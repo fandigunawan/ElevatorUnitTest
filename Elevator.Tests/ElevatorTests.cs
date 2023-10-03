3d52087e6fb5d5513ac306d9fcd661de4fc9afbb
Fandi Gunawan <10239907+fandigunawan@users.noreply.github.com>
2023-10-03 08:14:01 +0700

    Initial revision

---
+++ Elevator.Tests/ElevatorTests.cs
@@ -0,0 +1,163 @@
+using Elevator;
+
+namespace Elevator.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckMaxWeightAllowedReached_EmptyElevator_ReturnFalse()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+
+            // Act
+            var result = myElevator.CheckMaxWeightAllowedReached();
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CheckMaxWeightAllowedReached_80MaxWeightWith80WeightEmployer_ReturnTrue()
+        {
+            // Arrange
+            var myElevator = new Elevator(80);
+            // employee 1
+            var Programmer = new Employee();
+            Programmer.Weight = 80;
+
+            // Act
+            //adding employees to the elevator
+            myElevator.InUser(Programmer);
+            var result = myElevator.CheckMaxWeightAllowedReached();
+
+            // Assert            
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CheckMaxWeightAllowedReached_100MaxWeightWithSeveralEmployees_ReturnTrue()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+            // employee 1
+            var Director = new Employee();
+            Director.Weight = 75;
+            Director.IsExecutive = true;
+            // employee 2
+            var Producer = new Employee();
+            Producer.Weight = 85;
+
+            // Act
+            //adding employees to the elevator
+            myElevator.InUser(Director);
+            myElevator.InUser(Producer);
+            var result = myElevator.CheckMaxWeightAllowedReached();
+
+            // Assert            
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CheckMaxWeightAllowedReached_100MaxWeightWithSeveralEmployeesButSubtractingOne_ReturnFalse()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+            // employee 1
+            var Director = new Employee();
+            Director.Weight = 75;
+            Director.IsExecutive = true;
+            // employee 2
+            var Producer = new Employee();
+            Producer.Weight = 85;
+
+            // Act
+            //adding employees to the elevator
+            myElevator.InUser(Director);
+            myElevator.InUser(Producer);
+            // removing one
+            myElevator.OutUser(Producer);
+            var result = myElevator.CheckMaxWeightAllowedReached();
+
+            // Assert            
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void OutUser_SubtractingSeveralEmployeeWhoAreNotInTheElevator_CurrentWeightResult0()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+            // employee 1
+            var Artist = new Employee();
+            Artist.Weight = 75;
+            // employee 2
+            var GameDesigner = new Employee();
+            GameDesigner.Weight = 85;
+
+            // Act
+            // removing employees who aren't inside the elevator
+            myElevator.OutUser(Artist);
+            myElevator.OutUser(GameDesigner);
+
+            // Assert            
+            Assert.That(myElevator.CurrentWeight, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GoToVipSection_EmployeeWithVipPass_ReturnTrue()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+            // employee 
+            var CEO = new Employee();
+            CEO.Weight = 90;
+            CEO.IsExecutive = true;
+
+            // Act
+            // adding employee inside the elvator, and go to vip section
+            myElevator.InUser(CEO);
+            var result = myElevator.GoToVipSection(CEO);
+
+            // Assert            
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void GoToVipSection_EmployeeWithoutVipPass_ReturnFalse()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+            // employee 
+            var Guard = new Employee();
+            Guard.Weight = 90;
+
+            // Act
+            // adding employee inside the elvator, and go to vip section
+            myElevator.InUser(Guard);
+            var result = myElevator.GoToVipSection(Guard);
+
+            // Assert            
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void GoToVipSection_ThereAreNotEmployeesInTheElevator_ReturnFalse()
+        {
+            // Arrange
+            var myElevator = new Elevator(100);
+
+            // Act
+            // go to vip section            
+            var result = myElevator.GoToVipSection(new Employee());
+
+            // Assert            
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# CSharp Unit Testing and Automation
+
+Code is forked from [https://github.com/alexmuab/unit-testing-csharp-example](https://github.com/alexmuab/unit-testing-csharp-example)
